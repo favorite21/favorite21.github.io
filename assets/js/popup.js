abed264f35218532e86a0a389f5f8cfab17620dd
Dongkwan Kim <todoaskit@gmail.com>
2019-02-19 21:38:40 +0900

    Add recruiting popup

---
+++ assets/js/popup.js
@@ -0,0 +1,4 @@
+Swal.fire({
+  html:
+    '<a href="http://song.korea.ac.kr/Notice-Lab-Member-Recruiting(Post-doctoral-Fellowship-and-Graduate-Students)/"><image src="/assets/img/recruit.jpg"> </a>',
+})
